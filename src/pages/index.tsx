cb9f8a4947d30e4f6862ad80a07003a688cc7fda
Jack Driscoll <jdriscoll98@ufl.edu>
2022-12-30 09:40:14 -0500

    Update mobile styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,8 +16,8 @@ const Home: NextPage = () => {
         <meta name="description" content="Golf Tracker" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#233329] to-[#63D471]">
-        <div className="h-[90vh] w-[90vw] rounded-lg bg-white p-8 shadow-lg sm:h-[30rem] sm:w-[30rem]">
+      <main className="flex min-h-full flex-col items-center justify-center overflow-hidden bg-gradient-to-b from-[#233329] to-[#63D471]">
+        <div className="h-[85vh] w-[90vw] overflow-hidden rounded-lg bg-white p-8 shadow-lg sm:h-[30rem] sm:w-[30rem]">
           <div className="flex h-full w-full flex-col justify-between gap-[1rem] sm:justify-evenly">
             <h1 className="text-center text-4xl font-bold">Golf Tracker</h1>
 
